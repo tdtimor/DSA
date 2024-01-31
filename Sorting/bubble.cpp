93d728d2a6593b209a18b50093cd2515f1fcb881
tdtimor <dugeytanzin29@gmail.com>
2024-01-31 10:33:21 +0530

    a sorting algo with time complexity of O(n^2)

---
+++ Sorting/bubble.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(vector<int> &arr, int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    vector<int> arr;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        arr.push_back(x);
+    }
+    cout<<"Array before sorting:"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+    bubbleSort(arr, n);
+    cout<<"Array after sorting:"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
